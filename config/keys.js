9cecc3eadc6bcc2a37487427edd49453d629f795
zany <muguliebe@gmail.com>
2018-10-30 22:41:40 +0900

    mongo added

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://test:test13@ds145563.mlab.com:45563/temp"
+};
--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'))
+// DB config
+const db = require('../config/keys').mongoURI;
 
-const port = process.env.PORT || 3000
+// connect to mongodb
+mongoose.connect(db)
+  .then(() => console.log('mongo DB Connected'))
+  .catch(err => console.log(err))
+;
 
+// controller
+app.get('/', (req, res) => res.send('Hello World'));
+
+// server start
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
