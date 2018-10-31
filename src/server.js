9b6231d2294a91694ac8600ce2339edd7e3ef7c4
zany <muguliebe@gmail.com>
2018-11-01 06:54:39 +0900

    added jquery bootstrap

--- src/server.js
+++ src/server.js
@@ -41,10 +41,19 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 // controller
-app.get('/', (req, res) => res.send(`alive`));
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+// if (process.env.NODE_ENV === 'production') {
+// Set static folder
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// }
+
 
 // error handling
 app.use((err, req, res, next) => {
@@ -55,5 +64,5 @@ app.use((err, req, res, next) => {
 });
 
 // server start
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
