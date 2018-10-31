b37713cd84feb2b7b9455e0b51b8bf02e4a011b5
zany <muguliebe@gmail.com>
2018-11-01 07:29:03 +0900

    fontawesome cdn

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        react app 
+      <div className="App container-fluid">
+        <h1><i className="fas fa-ghost" /></h1>
+        react app
       </div>
     );
   }
