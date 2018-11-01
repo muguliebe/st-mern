92a534ce38455bc056a3777412f3c270a9f2a713
zany <muguliebe@gmail.com>
2018-11-01 09:19:57 +0900

    complete

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 class App extends Component {
   render() {
@@ -13,6 +15,10 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
           <Footer />
         </div>
       </Router>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
       <div className="container">
-        <a className="navbar-brand" href="/">What the</a>
+        <Link className="navbar-brand" to="/">What the</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
           <span className="navbar-toggler-icon" />
         </button>
@@ -12,17 +13,17 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="mobile-nav">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/"> Profile
-              </a>
+              <Link className="nav-link" to="/profile"> Profile
+              </Link>
             </li>
           </ul>
 
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/">Sign Up</a>
+              <Link className="nav-link" to="/register">Sign Up</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">Login</a>
+              <Link className="nav-link" to="/login">Login</Link>
             </li>
           </ul>
         </div>
