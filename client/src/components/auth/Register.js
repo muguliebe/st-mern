469ae733158281086acdba4311a02c77e6bf91c9
zany <muguliebe@gmail.com>
2018-11-02 03:54:20 +0900

    register ing

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Register() {
-  const name      = '';
-  const email     = '';
-  const password  = '';
-  const password2 = '';
-  const errors    = {};
+
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const handleChange = e => {
+    setUser({...user, [e.target.name]: e.target.value})
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(user)
+  };
 
   return (
     <div className="register">
@@ -14,26 +25,23 @@ export default function Register() {
           <div className="col-md-8 m-auto">
             <h1 className="display-4 text-center">Sign Up</h1>
             <p className="lead text-center">Create account</p>
-            <form action="create-profile.html">
+            <form onSubmit={handleSubmit}>
               <div className="form-group">
-                <input type="text" className="form-control form-control-lg"
-                       placeholder="Name" name="name" value={name} />
+                <input type="text" className="form-control form-control-lg" placeholder="Name"
+                       name="name" value={user.name} onChange={handleChange} />
               </div>
               <div className="form-group">
-                <input type="email" className="form-control form-control-lg"
-                       placeholder="Email Address" name="email" value={email} />
-                <small className="form-text text-muted">use a Gravatar email
-                </small>
+                <input type="email" className="form-control form-control-lg" placeholder="Email Address"
+                       name="email" value={user.email} onChange={handleChange} />
+                <small className="form-text text-muted">use a Gravatar email</small>
               </div>
               <div className="form-group">
-                <input type="password" className="form-control form-control-lg"
-                       placeholder="Password" name="password"
-                       value={password} />
+                <input type="password" className="form-control form-control-lg" placeholder="Password"
+                       name="password" value={user.password} onChange={handleChange} />
               </div>
               <div className="form-group">
-                <input type="password" className="form-control form-control-lg"
-                       placeholder="Confirm Password" name="password2"
-                       value={password2} />
+                <input type="password" className="form-control form-control-lg" placeholder="Confirm Password"
+                       name="password2" value={user.password2} onChange={handleChange} />
               </div>
               <input type="submit" className="btn btn-info btn-block mt-4" />
             </form>
