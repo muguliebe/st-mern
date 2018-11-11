0085781d56cdddbe923c55a93db9ecfb8d1f803f
zany <muguliebe@gmail.com>
2018-11-12 03:17:10 +0900

    change proxy port

--- client/src/setup.js
+++ client/src/setup.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const initBaseUrl = () => {
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 3001
   axios.defaults.baseURL = 'http://localhost:' + port
 }
