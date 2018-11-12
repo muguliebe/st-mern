fe4d9f855d5fe7215b23c06894ee71eda43c1789
zany <muguliebe@gmail.com>
2018-11-12 11:59:04 +0900

    modified proxy process

--- client/script/change-package.js
+++ client/script/change-package.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
+const path = require('path')
 
-fs.readFile('../package.json', 'utf8', function (err, data) {
+const file = path.resolve(__dirname, '../', 'package.json')
+fs.readFile(file, 'utf8', (err, data) => {
   if (err) return console.log(`Error with configure script: ${err}`)
 
   // parse current package.json into an object
@@ -10,6 +12,9 @@ fs.readFile('../package.json', 'utf8', function (err, data) {
   let proxy = process.env.REACT_APP_PROXY || 'http://localhost.com'
   proxy += ':'
   proxy += process.env.PORT || '3001'
+  console.log(`process.env.PORT=${process.env.PORT}`)
+  console.log(`bf proxy:${parsedData.proxy}`)
+  console.log(`af proxy:${proxy}`)
 
   parsedData.proxy = proxy
 
@@ -18,7 +23,7 @@ fs.readFile('../package.json', 'utf8', function (err, data) {
   let dataString = JSON.stringify(parsedData, null, spacing)
 
   // overwrite the current package.json
-  fs.writeFile('../package.json', dataString, err => {
+  fs.writeFile(file, dataString, err => {
     if (err) console.error(`Error writing file: ${err}`)
   })
 })
